d7bc5101ad7d6133dbc6d4c76cddb4a18ce18086
Liam Bowen <LiamBowen@gmail.com>
2016-03-18 17:17:07 -0400

    add my trace crap to the autoload_paths

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,9 @@
 require File.expand_path('../boot', __FILE__)
 
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "sprockets/railtie"
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'sprockets/railtie'
 
 Bundler.require(:default, Rails.env)
 
@@ -12,7 +12,7 @@ require_relative '../app/workers/update_scheduler'
 module RailsAssets
   class Application < Rails::Application
     config.cache_store = :redis_store, ENV['REDIS_URL']
-
+    config.autoload_paths << Rails.root.join('lib/trace')
     Rails.configuration.x.hostname = 'rails-assets.tenex.tech'
   end
 end
