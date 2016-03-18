d04f0c6d81c980bcd8ec8be28fb178d50c151181
Liam Bowen <LiamBowen@gmail.com>
2016-03-18 17:17:33 -0400

    rubocop + route to heap dump method

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,33 @@
 RailsAssets::Application.routes.draw do
   get '/packages' => 'main#packages'
   get '/packages/:name' => 'main#package',
-    constraints: { name: /[^\/]+/ }
+      constraints: { name: /[^\/]+/ }
 
   resources :components, only: [:index, :new, :create] do
     collection do
       get '/:name/rebuild' => 'components#rebuild',
-        constraints: { name: /[^\/]+/ }
+          constraints: { name: /[^\/]+/ }
 
       get '/:name/:version' => 'components#assets',
-        constraints: { version: /[^\/]+/, name: /[^\/]+/ }
+          constraints: { version: /[^\/]+/, name: /[^\/]+/ }
     end
   end
 
-  get "/api/v1/dependencies", to: "main#dependencies"
+  get '/api/v1/dependencies', to: 'main#dependencies'
   resources :donations, only: :create
 
   require 'sidekiq/web'
   Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
     [user, password] == [
-      "admin", ENV['SIDEKIQ_PASSWORD'] || "password"
+      'admin', ENV['SIDEKIQ_PASSWORD'] || 'password'
     ]
   end
   mount Sidekiq::Web => '/sidekiq'
 
   get '/status', to: 'main#status'
+  get '/dump', to: 'main#heap_dump'
 
-  root to: "main#home"
+  root to: 'main#home'
 
   get '/home', to: redirect('/')
 end
